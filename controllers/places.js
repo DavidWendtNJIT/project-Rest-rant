8a95cfe2046f64dbc87128716e10fe69ef602479
DavidWendtNJIT <djw5383@gmail.com>
2022-05-02 21:18:16 -0400

    Adding Bottstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -7,14 +7,15 @@ router.get("/", (req, res) => {
       city: "Seattle",
       state: "WA",
       cuisines: "Thai, Pan-Asian",
-      pic: "http://placekitten.com/250/250",
+      pic: "/images/rest2.jpg",
+      className: "rest2",
     },
     {
       name: "Coding Cat Cafe",
       city: "Phoenix",
       state: "AZ",
       cuisines: "Coffee, Bakery",
-      pic: "http://placekitten.com/250/250",
+      pic: "/images/rest1.jpg",
     },
   ];
 
--- views/error404.jsx
+++ views/error404.jsx
@@ -1,16 +1,19 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function error404 () {
+function error404() {
   return (
     <Def>
-        <main>
-            <h1>404: PAGE NOT FOUND</h1>
-            <p>Oops, sorry, we can't find this page!</p>
-        </main>
+      <main>
+        <div>
+          <img src="/images/404.jpg" alt="404 Error" />
+          <div>
+            Photo by <a href="https://www.pixar.com/sadDFaswdQ">PIXAR</a>
+          </div>
+        </div>
+      </main>
     </Def>
-  )
+  );
 }
 
-
-module.exports = error404
+module.exports = error404;

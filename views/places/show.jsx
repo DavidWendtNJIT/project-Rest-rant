2b77e576c5c7eae43468a50ed47b39842a713163
DavidWendtNJIT <djw5383@gmail.com>
2022-05-06 23:56:29 -0400

    Updated some styling

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -11,10 +11,10 @@ function show(data) {
               <img src={data.place.pic} class="img-responsive img-resize" />
             </div>
             <div className="col-md-5 my-2">
-              <h1>{data.place.name}</h1>
-              <h2 className="mt-2">Rating</h2>
+              <h1 className="rest-name">{data.place.name}</h1>
+              <h3 className="mt-2">Rating</h3>
               <p>No rating yet...</p>
-              <h2>Description</h2>
+              <h3>Description</h3>
               <p className="mt4">
                 Located in {data.place.city}, {data.place.state} serving{" "}
                 {data.place.cuisines}
@@ -25,7 +25,7 @@ function show(data) {
             <div className="col">
               <div className="my-3 py-3 card text-center">
                 <div className="card-body">
-                  <h2>Comments</h2>
+                  <h3>Comments</h3>
                   <p>No comments yet...</p>
                 </div>
               </div>
